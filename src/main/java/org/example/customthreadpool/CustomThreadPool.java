25bb4043c01f01c7adfd1806f78a0ab3db93b9c4
liangxiaole <xiaoleliang045@gmail.com>
2024-06-12 20:41:30 +0900

    custom connection pool

--- src/main/java/org/example/customthreadpool/CustomThreadPool.java
+++ src/main/java/org/example/customthreadpool/CustomThreadPool.java
@@ -2,6 +2,7 @@ package org.example.customthreadpool;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -23,6 +24,37 @@ class BlockingQueue<T> {
 
     // 5. 容量
     private int capacity;
+
+    // 带超时的阻塞获取
+    public T poll(long timeout, TimeUnit unit) {
+        lock.lock();
+        try {
+            // transfer to same time format
+            long nanos = unit.toNanos(timeout); // 转换为纳秒
+
+            // 如果队列中没任务，那么需要等待生产者生产任务
+            while (queue.isEmpty()) {
+                try {
+                    // 如果剩余等待时间已经耗尽，那么直接退出
+                    if (nanos <= 0) {
+                        return null;
+                    }
+
+                    // 此方法会返回等待的剩余时间，比如原本要等待5秒，但是在第3秒的时候被虚假唤醒，接下来还需要等待2秒，那么这个2秒就会被返回
+                    nanos = emptyWaitSet.awaitNanos(nanos);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            // 消费者消费了一个任务，给队列腾出来空间了，通知生产者那边生产任务
+            T t = queue.removeFirst();
+            fullWaitSet.signal();
+            return t;
+        } finally {
+            lock.unlock();
+        }
+    }
 
     // 阻塞获取
     public T take() {
